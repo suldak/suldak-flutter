e4e6b4884697792a8e6b036b92314e2997e1bbf1
choi-seung-min <3235tmdals@gmail.com>
2023-09-30 00:53:31 +0900

    naver, google login demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
@@ -5,6 +6,7 @@ import 'app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   KakaoSdk.init(nativeAppKey: '70a060e599c0641598be32da0f6790fc');
   String key = await KakaoSdk.origin;
   print("origin: " + key);
