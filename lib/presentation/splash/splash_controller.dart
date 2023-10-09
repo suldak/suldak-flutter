3a35455cb93e82f6a06f4c98a0446da346dd73c6
geuntae1 <dymnam@naver.com>
2023-10-09 22:52:25 +0900

    [refactor]: Shared -> Get Storage, "" -> '', text: ''.tr 적용

--- lib/presentation/splash/splash_controller.dart
+++ lib/presentation/splash/splash_controller.dart
@@ -1,5 +1,5 @@
 import 'package:get/get.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:get_storage/get_storage.dart';
 
 import '../../config/routes.dart';
 
@@ -7,19 +7,23 @@ class SplashController extends GetxController {
   static SplashController get to => Get.find();
 
   // Variable ▼ ------------------------------------------------------
+  final storage = GetStorage();
 
   // Functions ▼ ------------------------------------------------------
 
   void navigateOnboarding() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String? onboard = prefs.getString('onboard');
-    if (onboard == null) {
+    var onboardKey = getBoolData('onboard');
+    if (onboardKey == false) {
       await Future.delayed(const Duration(milliseconds: 3000));
       Get.offAllNamed(Routes.onboarding);
     } else {
       await Future.delayed(const Duration(milliseconds: 3000));
       Get.offAllNamed(Routes.login);
     }
+  }
+
+  bool getBoolData(String key) {
+    return storage.read(key) ?? false;
   }
 
   // Life Cycle ▼ ------------------------------------------------------
