819177c0415fb1c781da678254f4d37e984e1dd0
geuntae1 <dymnam@naver.com>
2023-11-01 17:48:26 +0900

    [refactor]: DateFormat을 다른 곳에서도 유용하게 사용하기 위해 util 라이브러리로 분리

---
+++ lib/utils/date_formater.dart
@@ -0,0 +1,15 @@
+class DateFormat {
+  String formatDate(DateTime date) {
+    final now = DateTime.now();
+    final difference = now.difference(date);
+    if (difference.inDays > 0) {
+      return '${difference.inDays}일 전';
+    } else if (difference.inHours > 0) {
+      return '${difference.inHours}시간 전';
+    } else if (difference.inMinutes > 0) {
+      return '${difference.inMinutes}분 전';
+    } else {
+      return '방금 전';
+    }
+  }
+}
